4a127ab164396707da54b07aa5662d1649fd45f4
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-26 12:20:41 -0300

    ✨ feat: Create Base Styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      '900': '#121214',
+      '850': '#272727',
+      '800': '#202024',
+      '750': '#2C2E32',
+      '700': '#323239',
+      '600': '#3a3a44',
+      '500': '#44444e',
+      '400': '#565663',
+      '300': '#c4c4cc',
+      '200': '#cccccc',
+      '150': '#DADADA',
+      '100': '#e1e1e6',
+      '50': '#F5F8FA',
+    },
+    black: {
+      '900': '#000000',
+    },
+    white: {
+      '900': '#FFFFFF',
+    },
+    green: {
+      '500': '#00875f',
+      '300': '#00b37e',
+    },
+    blue: {
+      '400': '#2684ff',
+    },
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.750',
+        color: 'gray.100',
+      },
+    },
+  },
+});
