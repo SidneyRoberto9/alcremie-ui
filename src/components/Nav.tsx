5541bd5039b3cc4aa984b50828b4d7a7cfdff671
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-26 15:36:07 -0300

    ✨ feat: Create Nav Component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,86 @@
+import { Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { HouseLine, Image, UploadSimple } from 'phosphor-react';
+import { slide as Menu } from 'react-burger-menu';
+
+import { useNav } from '../context/useNav';
+import { NavLink } from './NavLink';
+
+interface routesProps {
+  path: string;
+  icon: JSX.Element;
+  name: string;
+}
+
+export function Nav() {
+  const { toggleNav, isOpen } = useNav();
+  const { pathname } = useRouter();
+
+  const routes: routesProps[] = [
+    {
+      path: '/home',
+      icon: <HouseLine size={35} />,
+      name: 'home',
+    },
+    {
+      path: '/recent',
+      icon: <Image size={35} />,
+      name: 'recent',
+    },
+    {
+      path: '/upload',
+      icon: <UploadSimple size={35} />,
+      name: 'upload',
+    },
+  ];
+
+  return (
+    <Box
+      __css={{
+        '.bm-item-list': {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        },
+
+        '.bm-cross': {
+          cursor: 'pointer',
+          backgroundColor: 'gray.300',
+        },
+
+        '.bm-menu': {
+          background: 'gray.850',
+          padding: '0.3rem 0.3rem 0',
+          fontSize: '1.15rem',
+          WebkitBoxShadow: '9px 0px 5px -3px rgba(0,0,0,0.48)',
+          MozBoxShadow: '9px 0px 5px -3px rgba(0,0,0,0.48)',
+          boxShadow: '9px 0px 5px -3px rgba(0,0,0,0.48)',
+        },
+
+        '.bm-overlay': {
+          background: 'rgba(0, 0, 0, 0.3)',
+        },
+      }}
+    >
+      <Menu
+        right={false}
+        isOpen={isOpen}
+        width={'25rem'}
+        customBurgerIcon={false}
+        onStateChange={(state) => toggleNav(state.isOpen)}
+      >
+        <Box
+          paddingTop={'3.75rem'}
+          width={'100%'}
+          display={'grid !important'}
+          gridTemplateRows={'1fr'}
+          gridGap={'0.5rem'}
+        >
+          {routes.map((route, index) => (
+            <NavLink {...route} key={index} />
+          ))}
+        </Box>
+      </Menu>
+    </Box>
+  );
+}
