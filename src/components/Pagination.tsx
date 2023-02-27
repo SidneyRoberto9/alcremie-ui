fd4d1a2459038a304fc4dc2666b1a481cdb32e93
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-27 09:52:13 -0300

    ✨ feat: Create and use Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,123 @@
+import { Button, Flex, Text } from '@chakra-ui/react';
+import { useContextSelector } from 'use-context-selector';
+
+import { galleryContext } from '../context/useGallery';
+
+export function Pagination() {
+  const {
+    contentTotalSize,
+    loading,
+    pageSize,
+    pageNumber,
+    onNextPage,
+    onPreviousPage,
+  } = useContextSelector(
+    galleryContext,
+    ({
+      contentTotalSize,
+      loading,
+      pageSize,
+      pageNumber,
+      onNextPage,
+      onPreviousPage,
+    }) => ({
+      contentTotalSize,
+      loading,
+      pageSize,
+      pageNumber,
+      onNextPage,
+      onPreviousPage,
+    }),
+  );
+
+  const totalPages =
+    contentTotalSize == 0
+      ? Math.ceil(1 / pageSize)
+      : Math.ceil(contentTotalSize / pageSize);
+
+  const isLastPage = pageNumber === totalPages;
+
+  const disabledNextButton = isLastPage || loading;
+  const disabledPreviousButton = pageNumber == 1 || loading;
+
+  return (
+    <Flex
+      position={'fixed'}
+      bottom={'1.25rem'}
+      left={'50%'}
+      width={'15rem'}
+      height={'2.5rem'}
+      transform={'translate(-50%, 0%)'}
+      bg={'gray.500'}
+      alignItems={'center'}
+      justifyContent={'space-between'}
+      borderRadius={'4px'}
+    >
+      <Button
+        onClick={onPreviousPage}
+        isDisabled={disabledPreviousButton}
+        outline={'none'}
+        border={'none'}
+        width={'5.35rem'}
+        borderRadius={'4px'}
+        height={'2.35rem'}
+        margin={'0.15rem'}
+        fontSize={'1rem'}
+        fontWeight={'500'}
+        color={'green.300'}
+        bg={'gray.700'}
+        transition={'filter 200ms ease-in-out'}
+        cursor={'pointer'}
+        textTransform={'capitalize'}
+        _hover={{
+          filter: 'brightness(0.7)',
+        }}
+        _disabled={{
+          cursor: 'not-allowed',
+          filter: 'brightness(0.5)',
+        }}
+      >
+        previous
+      </Button>
+
+      <Text
+        display={'block'}
+        padding={'0.375rem 0.75rem'}
+        fontSize={'1.25rem'}
+        color={'white.900'}
+        lineHeight={'1.5'}
+        userSelect={'none'}
+        pointerEvents={'none'}
+      >
+        {`${pageNumber}/${totalPages || 1}`}
+      </Text>
+
+      <Button
+        onClick={onNextPage}
+        isDisabled={disabledNextButton}
+        outline={'none'}
+        border={'none'}
+        width={'5.35rem'}
+        borderRadius={'4px'}
+        height={'2.35rem'}
+        margin={'0.15rem'}
+        fontSize={'1rem'}
+        fontWeight={'500'}
+        color={'green.300'}
+        bg={'gray.700'}
+        transition={'filter 200ms ease-in-out'}
+        cursor={'pointer'}
+        textTransform={'capitalize'}
+        _hover={{
+          filter: 'brightness(0.7)',
+        }}
+        _disabled={{
+          cursor: 'not-allowed',
+          filter: 'brightness(0.5)',
+        }}
+      >
+        next
+      </Button>
+    </Flex>
+  );
+}
--- src/pages/recent.tsx
+++ src/pages/recent.tsx
@@ -1,7 +1,28 @@
 import { Box } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useContextSelector } from 'use-context-selector';
 
-export default function Recent() {
+import { GalleryFetchDataResponse } from '../@types/gallery';
+import { Pagination } from '../components/Pagination';
+import { galleryContext } from '../context/useGallery';
+import { api } from '../server/api';
+
+interface RecentProps {
+  content: GalleryFetchDataResponse;
+}
+
+export default function Recent({ content }: RecentProps) {
+  const setContent = useContextSelector(
+    galleryContext,
+    ({ setContent }) => setContent,
+  );
+
+  useEffect(() => {
+    setContent(content);
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,8 +30,16 @@ export default function Recent() {
       </Head>
 
       <Box paddingTop={'3.75rem'}>
-        <a href="'/4040'">test</a>
+        <Pagination />
       </Box>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await api.get<GalleryFetchDataResponse>(`/img/0?all=true`);
+
+  return {
+    props: { content: data },
+  };
+};
