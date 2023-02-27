bfad3df27cdac9ff19a8427c8f4bf4094feda60b
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-27 09:52:55 -0300

    ✨ feat: Add Providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,21 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { Header } from '../components/Header';
 import { Nav } from '../components/Nav';
+import { GalleryProvider } from '../context/useGallery';
 import { NavProvider } from '../context/useNav';
 import { theme } from '../styles/theme';
 
 import type { AppProps } from 'next/app';
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <NavProvider>
-        <Nav />
-        <Header />
-        <Component {...pageProps} />
-      </NavProvider>
+      <GalleryProvider>
+        <NavProvider>
+          <Nav />
+          <Header />
+          <Component {...pageProps} />
+        </NavProvider>
+      </GalleryProvider>
     </ChakraProvider>
   );
 }
