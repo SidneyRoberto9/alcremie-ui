a31861cecb4e09d3bff571ab6761b4f20fd5377f
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-27 10:47:36 -0300

    ✨ feat: Setup LoadingPage Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 import { Header } from '../components/Header';
+import { LoadingPage } from '../components/LoadingPage';
 import { Nav } from '../components/Nav';
 import { GalleryProvider } from '../context/useGallery';
 import { NavProvider } from '../context/useNav';
@@ -8,8 +11,32 @@ import { theme } from '../styles/theme';
 
 import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleRouteChangeStart() {
+    setIsLoading(true);
+  }
+
+  function handleRouteChangeStop() {
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeStop);
+    router.events.on('routeChangeError', handleRouteChangeStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeStop);
+      router.events.off('routeChangeError', handleRouteChangeStop);
+    };
+  }, [router]);
+
   return (
     <ChakraProvider theme={theme}>
+      <LoadingPage isLoading={isLoading} />
       <GalleryProvider>
         <NavProvider>
           <Nav />
