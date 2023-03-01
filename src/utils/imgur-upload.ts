d815f88753d4b4b0838532ff092e106281df3faa
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-01 03:21:37 -0300

    ✨ feat: Create a util responsible to upload images to imgur and save in database

---
+++ src/utils/imgur-upload.ts
@@ -0,0 +1,22 @@
+import sharp from 'sharp';
+
+import { imgur } from '../server/imgur';
+
+export async function imgurUpload(buf: Buffer) {
+  const sharpBuf = await sharp(buf).jpeg({ quality: 90 }).toBuffer();
+
+  //generate new album to up for production
+  const { data } = await imgur.upload({
+    image: sharpBuf,
+    type: 'stream',
+    album: 'QV3N6YY90tu0HPk',
+  });
+
+  const ImgurData = {
+    id: data.id,
+    deleteHash: data.deletehash as string,
+    link: data.link,
+  };
+
+  return { ImgurData };
+}
