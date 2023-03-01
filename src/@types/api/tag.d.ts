71ad7a254b01ca51a9b13fd5340ecfb7e295cff6
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-01 03:16:21 -0300

    ✨ feat: Create types for tag content in serverSide

---
+++ src/@types/api/tag.d.ts
@@ -0,0 +1,16 @@
+export interface TagProps {
+  id?: string;
+  name: string;
+  description: string;
+  is_nsfw: boolean;
+}
+
+export interface createTagDto {
+  name: string;
+  description: string;
+  is_nsfw: boolean;
+}
+
+export interface TagIds {
+  id: string;
+}
