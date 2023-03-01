388ada6bd3e4358f611c5632eb7216db6b34e827
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-01 03:21:55 -0300

    ✨ feat: Create a simple validate for isEmpty value

---
+++ src/utils/valitation.ts
@@ -0,0 +1,10 @@
+export function isEmpty(value: unknown): boolean {
+  return (
+    value === undefined ||
+    value === null ||
+    value === '' ||
+    value === false ||
+    value === 'undefined' ||
+    value === 'null'
+  );
+}
