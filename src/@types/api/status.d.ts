207ea60efbabb0cc97a7475948405ba0ea30ed2d
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-02 12:36:09 -0300

    ✨ feat: Create status type for statistics data

---
+++ src/@types/api/status.d.ts
@@ -0,0 +1,12 @@
+export interface Statistics {
+  id: string;
+  totalImages: number;
+  totalTags: number;
+  totalRequests: number;
+}
+
+export interface StatisticsDto {
+  tags: number;
+  images: number;
+  requests: number;
+}
