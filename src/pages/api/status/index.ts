c5979629af4f2e4dbf623ffe9c2fac8dddaa4987
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-02 12:38:00 -0300

    ✨ feat: Create new Request for statistics

---
+++ src/pages/api/status/index.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+
+import { statisticsToDto } from '../../../utils/converter-data';
+import { addRequest, getStatistics } from '../../../utils/statistic-query';
+
+const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end('Something broke!');
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end('Page is not found');
+  },
+});
+
+apiRoute.get(async (req, res) => {
+  await addRequest();
+
+  const statistics = await getStatistics();
+
+  if (!statistics) {
+    return res.status(404).json({
+      message: 'No statistics found',
+    });
+  }
+
+  res.status(200).json(statisticsToDto(statistics));
+});
+
+export default apiRoute;
