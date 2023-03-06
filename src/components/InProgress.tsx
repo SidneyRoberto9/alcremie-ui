73ab340bb5825e01d3db96128f0ecc17bab94107
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-06 14:40:54 -0300

    ✨ feat: Create and use InProgress component

---
+++ src/components/InProgress.tsx
@@ -0,0 +1,27 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { Barricade } from 'phosphor-react';
+
+import { TextTitle } from './TextTitle';
+
+export function InProgress() {
+  return (
+    <Box
+      position={'absolute'}
+      top={'50%'}
+      left={'50%'}
+      transform={'translate(-50%, -50%)'}
+      bg={'gray.850'}
+      borderRadius={'1rem'}
+    >
+      <Flex
+        align={'center'}
+        justify={'center'}
+        flexDirection={'column'}
+        padding={'3rem 5rem'}
+      >
+        <Barricade size={300} />
+        <TextTitle>In Progress...</TextTitle>
+      </Flex>
+    </Box>
+  );
+}
--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,9 +1,7 @@
-import { Box, Flex } from '@chakra-ui/react';
 import { NextSeo } from 'next-seo';
-import { Barricade } from 'phosphor-react';
 
 import { Content } from '../components/Content';
-import { TextTitle } from '../components/TextTitle';
+import { InProgress } from '../components/InProgress';
 
 export default function Docs() {
   return (
@@ -11,24 +9,7 @@ export default function Docs() {
       <NextSeo title="Docs | Alcremie" />
 
       <Content>
-        <Box
-          position={'absolute'}
-          top={'50%'}
-          left={'50%'}
-          transform={'translate(-50%, -50%)'}
-          bg={'gray.850'}
-          borderRadius={'1rem'}
-        >
-          <Flex
-            align={'center'}
-            justify={'center'}
-            flexDirection={'column'}
-            padding={'3rem 5rem'}
-          >
-            <Barricade size={300} />
-            <TextTitle>In Progress...</TextTitle>
-          </Flex>
-        </Box>
+        <InProgress />
       </Content>
     </>
   );
