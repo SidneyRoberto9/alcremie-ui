1d3c9135f5f8e3eb631d7b95e969c1457bf1db29
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-05 23:04:20 -0300

    ✨ feat: Create manager tag page

--- src/pages/tag/manager.tsx
+++ src/pages/tag/manager.tsx
@@ -1,27 +1,67 @@
-import { Box } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import { NextSeo } from 'next-seo';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 import { Content } from '../../components/Content';
+import { TableTags } from '../../components/Manager';
+import { AddModal } from '../../components/Manager/AddModal';
+import { useTags } from '../../context/useTags';
 
 export default function Manager() {
+  const { getTags, data, filterTag } = useTags();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const search = e.target.value.toLowerCase();
+
+    filterTag(search);
+  }
+
+  useEffect(() => {
+    getTags();
+  }, []);
+
   return (
     <>
       <NextSeo title="Tag Manager | Alcremie" />
+      <AddModal isOpen={isOpen} onClose={onClose} />
 
       <Content>
-        <Box
-          position={'absolute'}
-          top={'50%'}
-          left={'50%'}
-          transform={'translate(-50%, -50%)'}
-          width={{ base: '90%', md: '40%' }}
-          bg={'gray.850'}
-          borderRadius={'8px'}
+        <Flex
+          w={'100%'}
+          padding={'3rem'}
+          gap={'25rem'}
+          align={'center'}
+          justifyContent={'space-between'}
         >
-          casa
-        </Box>
+          <InputGroup>
+            <InputLeftAddon
+              children="Search"
+              color={'black'}
+              fontWeight={'bold'}
+            />
+            <Input
+              type="text"
+              placeholder="Search for a tag..."
+              onChange={handleSearch}
+            />
+          </InputGroup>
+
+          <Button variant={'default'} p={'0 2rem'} onClick={onOpen}>
+            Add Tag
+          </Button>
+        </Flex>
+
+        <TableTags data={data} />
       </Content>
     </>
   );
