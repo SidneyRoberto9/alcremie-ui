c7090bc5e6c05402797f728bc73a12f1fde1e809
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-03-05 23:03:21 -0300

    ✨ feat: Improve tags function in useTags

--- src/context/useTags.tsx
+++ src/context/useTags.tsx
@@ -6,6 +6,15 @@ import { api } from '../server/api';
 
 interface TagsProps {
   data: Tag[];
+  isLoading: boolean;
+  filterTag: (search: string) => void;
+  getTags: () => Promise<void>;
+  deleteTag: (id: string) => Promise<void>;
+  createTag: (
+    name: string,
+    description: string,
+    isNsfw: boolean,
+  ) => Promise<void>;
 }
 
 interface ContextProps {
@@ -16,11 +25,41 @@ const tagsContext = createContext({} as TagsProps);
 
 export function TagsContextProvider({ children }: ContextProps) {
   const [tags, setTags] = useState<Tag[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function filterTag(search: string) {
+    if (search.length < 2) {
+      return getTags();
+    }
+
+    setTags((state) =>
+      state.filter((tag) => tag.name.toLowerCase().includes(search)),
+    );
+  }
 
   async function getTags() {
+    setIsLoading(true);
     const { data } = await api.get('/tag');
 
     setTags(data.tags);
+    setIsLoading(false);
+  }
+
+  async function createTag(name: string, description: string, isNsfw: boolean) {
+    setIsLoading(true);
+    await api.post('/tag', {
+      name,
+      description,
+      is_nsfw: isNsfw,
+    });
+
+    await getTags();
+  }
+
+  async function deleteTag(id: string) {
+    setIsLoading(true);
+    await api.delete(`/tag/${id}`);
+    await getTags();
   }
 
   useEffect(() => {
@@ -31,6 +70,11 @@ export function TagsContextProvider({ children }: ContextProps) {
     <tagsContext.Provider
       value={{
         data: tags,
+        isLoading,
+        getTags,
+        createTag,
+        deleteTag,
+        filterTag,
       }}
     >
       {children}
